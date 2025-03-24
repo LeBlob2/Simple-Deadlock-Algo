f3e12746987d25d1dd0d47a796b5cb7880656ede
LeBlob2 <leblob2@proton.me>
2025-03-24 14:10:59 +0000

    more flavor text

--- Deadlock Bankers algorithm.cpp
+++ Deadlock Bankers algorithm.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
 #define MAX 20
 using namespace std;
+//This is still some what WIP and some notes are incomplete
 
 class bankers
 {
-private:
+private: // setup all the variables (Do i need to say what these are? Delete when read)
     int al[MAX][MAX], m[MAX][MAX], n[MAX][MAX], avail[MAX];
     int nop, nor, k, result[MAX], pnum, work[MAX], finish[MAX];
-
+    /* al = allocated resource | m = max resource demanded | n = needed resoures to complete the task | avail = available resources in the system */
+    /* nop = number of processes | nor = number of resource types | k = tracks the current process number for safe sequence | results = safe sequence | 
+       pnum = checks if the current process number can be executed or not | work = freed up resources that is added to available | finish = track if a process has finished execution*/
 public:
     bankers();
     void input();
@@ -17,7 +20,7 @@ public:
 };
 
 bankers::bankers()
-{
+{ //Initialize all the variables to zero to prevent it from reading garbage data
     k = 0;
     for (int i = 0;i < MAX;i++)
     {
@@ -28,7 +31,7 @@ bankers::bankers()
             n[i][j] = 0;
         }
         avail[i] = 0;
-        result[i] = 0;
+        result[i] = 0; 
         finish[i] = 0;
     }
 }
@@ -39,7 +42,7 @@ void bankers::input()
     //This part is for processes ie number of processes P1 P2 P3 P4 P5 like that
     cout << "Enter the number of processes:";
     cin >> nop;
-    //This is the number of resource types ie Resource A B C
+    //This is the needed resources like memory and ram and other idk so resource types will be Resource A B C
     cout << "Enter the number of resources:";
     cin >> nor;
     /*This allocates resource for each block ie P1 has 3 resource tyes needed A B C so Resource A has 1 B has 0 and C has 1 allocated to it so this will be subtracted later */
@@ -53,6 +56,7 @@ void bankers::input()
             cin >> al[i][j];
         }
     }
+    // This is the needed resources like memory and ram and other idk so 
     cout << "Enter the maximum resources that are needed for each process: " << endl;
     for (i = 0;i < nop;i++)
     {
@@ -64,6 +68,7 @@ void bankers::input()
             n[i][j] = m[i][j] - al[i][j];
         }
     }
+    //This is the available amount of resource types that is currently in the system for example 2 for A 3 for B and 1 for C
     cout << "Enter the currently available resources in the system: ";
     for (j = 0;j < nor;j++)
     {
@@ -76,11 +81,11 @@ void bankers::input()
 }
 
 void bankers::method()
-{
+{ // This is the main system to find a safe sequence
     int i = 0, j, flag;
     while (1)
     {
-        if (finish[i] == 0)
+        if (finish[i] == 0) // checks if the process is done if not redo it
         {
             pnum = search(i);
             if (pnum != -1)
@@ -115,14 +120,15 @@ void bankers::method()
 int bankers::search(int i)
 {
     int j;
-    for (j = 0;j < nor;j++)
-        if (n[i][j] > avail[j])
-            return -1;
+    for (j = 0;j < nor;j++) 
+        if (n[i][j] > avail[j]) // check if the needed resource is greater that the available resource
+            return -1; 
     return 0;
 }
 
 void bankers::display()
 {
+    //Output all the results into matrixes in their respetive tables 
     int i, j;
     cout << endl << "OUTPUT:";
     cout << endl << "========";
@@ -145,12 +151,13 @@ void bankers::display()
             cout << n[i][j] << "  ";
         }
     }
+    //This is the safe process sequence it means that the system will not encounter a deadlock scenario
     cout << "\nThe sequence of the safe processes are: \n";
     for (i = 0;i < k;i++)
     {
         int temp = result[i] + 1;
         cout << "P" << temp << " ";
-    }
+    }// Opposite of the top this is the unsafe sequence which might result in a deadlock
     cout << "\nThe sequence of unsafe processes are: \n";
     int flg = 0;
     for (i = 0;i < nop;i++)
@@ -163,16 +170,23 @@ void bankers::display()
     }
     cout << endl << "RESULT:";
     cout << endl << "=======";
-    if (flg == 1)
+    if (flg == 1) { // check if the best possible route is safe or not
         cout << endl << "The system is not in safe state and deadlock may occur!!";
-    else
+        cout << endl;
+        cout << endl;
+    }
+    else{
         cout << endl << "The system is in safe state and deadlock will not occur!!";
+        cout << endl;
+        cout << endl;
+    }
 }
 
 int main()
 {
-    cout << " DEADLOCK BANKER’S ALGORITHM " << endl;
+    cout << " DEADLOCK BANKERâ€™S ALGORITHM " << endl;
     bankers B;
+    //calls all the previous methods
     B.input();
     B.method();
     B.display();
